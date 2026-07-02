d1ae5fcf3f444038b3fd6e92a6bd6542535eebdb
videirajatai <videirajatai@users.noreply.github.com>
2026-07-02 18:56:28 +0000

    deploy: 70384a0c8f8a4432e2e7d5747ff428eb45335b6b

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js - Configurações centralizadas do Sistema Videira
+// ATUALIZE ESTE ARQUIVO PARA ALTERAR CHAVES EM TODOS OS PAINÉIS
+
+const SUPABASE_URL = 'https://ryvfogmfpjyamgqsltks.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5dmZvZ21mcGp5YW1ncXNsdGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5OTI2NTksImV4cCI6MjA4ODU2ODY1OX0.FSqv0YJwJH5-2Cl4ryhz6o3u7dzfzjD1MOjz2Di1_DA';
+
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxnystn3UqAKnfLUgw7tXS8CzrnpxTS0iax80rhMCRs-T47ibodGYvbRw2KRrtsnCzn/exec';
+
+// Para manter compatibilidade com código existente, também exportamos com nomes antigos
+const SB_URL = SUPABASE_URL;
+const supabseUrl = SUPABASE_URL;
+const SB_KEY = SUPABASE_ANON_KEY;
+const urlBackend = GOOGLE_SCRIPT_URL;
